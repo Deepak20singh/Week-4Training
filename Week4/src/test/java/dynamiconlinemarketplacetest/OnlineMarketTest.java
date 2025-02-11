6d47486c065d15785f24ec602821e9e1f9d622c9
Deepak <singh017deepak@gmail.com>
2025-02-11 11:53:50 +0530

    [Deepak] Add. Code of Online Market place with its test file

---
+++ Week4/src/test/java/dynamiconlinemarketplacetest/OnlineMarketTest.java
@@ -0,0 +1,23 @@
+package dynamiconlinemarketplacetest;
+
+import dynamiconlinemarketplace.Clothing;
+import dynamiconlinemarketplace.Discount;
+import dynamiconlinemarketplace.Product;
+import dynamiconlinemarketplace.Storage;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class OnlineMarketTest {
+    @Test
+    public void tester1(){
+        Clothing clothing1=new Clothing("Rymonds",250000);
+        Discount<Product> discount=new Discount<>();
+        discount.addDiscount(clothing1,23);
+        dynamiconlinemarketplace.Storage<Clothing> clothingStorage=new Storage<>();
+        clothingStorage.addItem(clothing1);
+
+        Assert.assertEquals(1,clothingStorage.getList().size());
+    }
+
+
+}
