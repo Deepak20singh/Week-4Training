8309919554a7ee8fe576a64a1eabccddff4423dd
Deepak <singh017deepak@gmail.com>
2025-02-14 14:35:20 +0530

    [Deepak] Add. Code of division using nested try catch with its test file

---
+++ Week4Day-4/src/test/java/exceptiontest/nestedtrytest/NestedTest.java
@@ -0,0 +1,30 @@
+package exceptiontest.nestedtrytest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static exceptionhandling.nestedtrycatch.OperationsArray.checkArray;
+
+public class NestedTest {
+    @Test
+    public void Tester(){
+        int[] arr= new int[5];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=i+1;
+
+        }
+        int index=3;
+        int no=2;
+        int number=checkArray(arr,index);
+
+        int ans = 0;
+        try{
+            ans=number/no;
+            System.out.println("The result :- "+ans);
+        }catch (ArithmeticException e){
+            System.out.println("Please enter number greater than zero");
+
+        }
+        Assert.assertEquals(2,ans);
+    }
+}
