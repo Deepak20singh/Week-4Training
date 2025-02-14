6899836a83af408d5aaa676d6597d846c014423a
Deepak <singh017deepak@gmail.com>
2025-02-14 14:32:55 +0530

    [Deepak] Add. Code of bank transaction with its test file

---
+++ Week4Day-4/src/test/java/exceptiontest/banktransactiontest/BankTest.java
@@ -0,0 +1,25 @@
+package exceptiontest.banktransactiontest;
+
+import exceptionhandling.banktransactionsystem.BankAccountSystem;
+import exceptionhandling.banktransactionsystem.InsufficientBalanceException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BankTest {
+    @Test
+    public void Tester() throws InsufficientBalanceException {
+        BankAccountSystem customer1;
+        double amount=2000;
+        if(amount>0){
+            customer1=new BankAccountSystem("Deepak","Acc0137913",amount);
+            double withdraw=1500;
+            customer1.withdrawl(withdraw);
+        }
+        else{
+            throw new IllegalArgumentException("Invalid amount!");
+        }
+        double current =customer1.getAmount();
+        Assert.assertEquals(500,current,0.01);
+
+    }
+}
